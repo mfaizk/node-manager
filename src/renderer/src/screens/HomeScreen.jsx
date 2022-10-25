41ca7d56f61eb006efe793f50a1de769c94e4a13
mfaizk <suneater1413@gmail.com>
2022-10-25 14:30:56 +0530

    initial code clean up

---
+++ src/renderer/src/screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <div style={{ backgroundColor: 'red' }}>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default HomeScreen
